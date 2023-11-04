8bbb97147e4722d01456014f5f6981517f04d5c8
xxxmoney <xxxmoney111@gmail.com>
2023-11-04 15:37:54 +0100

    Added web project.

---
+++ GamesTournamentsWeb.Web/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamesTournamentsWeb.Web.Controllers;
+
+//[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class BaseController : ControllerBase
+{
+    
+}
