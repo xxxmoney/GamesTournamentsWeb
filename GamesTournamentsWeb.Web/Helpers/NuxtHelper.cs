898df6df1f9de57a4f44f854772db464c2b17bf5
Jakub Hána <61245332+xxxmoney@users.noreply.github.com>
2023-11-04 08:39:50 -0700

    Merge task-web-project to main (#1)
    * Added web project.
    
    * Updated gitignore.
    
    * Added nuxt js project.
    
    * Moved client app, nuxt helper, changes.
    
    * Readded client app.

---
+++ GamesTournamentsWeb.Web/Helpers/NuxtHelper.cs
@@ -0,0 +1,100 @@
+﻿namespace GamesTournamentsWeb.Web.Helpers;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.SpaServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+
+public static class NuxtHelper
+{
+   private static Uri DevelopmentServerEndpoint { get; } = new Uri($"http://localhost:{Constants.ClientAppPort}");
+   private static TimeSpan Timeout { get; } = TimeSpan.FromSeconds(30);
+   // done message of 'npm run dev' command.
+   private const string DoneMessage = "DONE Compiled successfully in";
+
+   public static void UseNuxtDevelopmentServer(this ISpaBuilder spa)
+   {
+       spa.UseProxyToSpaDevelopmentServer(async () =>
+       {
+           var loggerFactory = spa.ApplicationBuilder.ApplicationServices.GetService<ILoggerFactory>();
+           var logger = loggerFactory.CreateLogger("Nuxt");
+           // if 'npm dev' command was executed yourself, then just return the endpoint.
+           if (IsRunning())
+           {
+              return DevelopmentServerEndpoint;
+           }
+           
+           string currentDirectory = Directory.GetCurrentDirectory();
+           
+           // launch Nuxt development server
+           var processInfo = new ProcessStartInfo
+           {
+              FileName = "cmd",
+              Arguments = $"/c {Constants.ClientAppRunCommand}",
+              WorkingDirectory = Path.Combine(currentDirectory, Constants.ClientAppFolder),
+              RedirectStandardError = true,
+              RedirectStandardInput = true,
+              RedirectStandardOutput = true,
+              UseShellExecute = false,
+           };
+           var process = Process.Start(processInfo);
+           var tcs = new TaskCompletionSource<int>();
+           _ = Task.Run(() =>
+           {
+              try
+              {
+                  string line;
+                  while ((line = process.StandardOutput.ReadLine()) != null)
+                  {
+                      logger.LogInformation(line);
+                      if (!tcs.Task.IsCompleted && line.Contains(DoneMessage))
+                      {
+                          tcs.SetResult(1);
+                      }
+                  }
+              }
+              catch (EndOfStreamException ex)
+              {
+                  logger.LogError(ex.ToString());
+                  tcs.SetException(new InvalidOperationException("Command failed.", ex));
+              }
+           });
+           _ = Task.Run(() =>
+           {
+              try
+              {
+                  string line;
+                  while ((line = process.StandardError.ReadLine()) != null)
+                  {
+                      logger.LogError(line);
+                  }
+              }
+              catch (EndOfStreamException ex)
+              {
+                  logger.LogError(ex.ToString());
+                  tcs.SetException(new InvalidOperationException("Command failed.", ex));
+              }
+           });
+           var timeout = Task.Delay(Timeout);
+           if (await Task.WhenAny(timeout, tcs.Task) == timeout)
+           {
+              throw new TimeoutException();
+           }
+           return DevelopmentServerEndpoint;
+       });
+   }
+
+   private static bool IsRunning() => IPGlobalProperties.GetIPGlobalProperties()
+           .GetActiveTcpListeners()
+           .Select(x => x.Port)
+           .Contains(Constants.ClientAppPort);
+}
+
---
+++ GamesTournamentsWeb.Web/Program.cs
@@ -0,0 +1,63 @@
+using GamesTournamentsWeb.Web;
+using GamesTournamentsWeb.Web.Helpers;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "API",
+        Description = ""
+    });
+
+    options.CustomSchemaIds(type => type.ToString());
+});
+
+builder.Services.AddSpaStaticFiles(options => options.RootPath = $"{Constants.ClientAppFolder}/dist");
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyMethod().AllowAnyOrigin();
+        });
+});
+
+var app = builder.Build();
+
+app.UseCors();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseSpaStaticFiles();
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = Constants.ClientAppFolder;
+    if (app.Environment.IsDevelopment())
+    {
+        // Launch development server for Nuxt
+        spa.UseNuxtDevelopmentServer();
+    }
+});
+
+app.Run();
