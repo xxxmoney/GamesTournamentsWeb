b337c82b88a0ecd02ba2d1aa871bb935febca05c
xxxmoney <xxxmoney111@gmail.com>
2023-11-04 16:34:50 +0100

    Moved client app, nuxt helper, changes.

---
+++ GamesTournamentsWeb.Web/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace GamesTournamentsWeb.Web;
+
+public class Constants
+{
+    public const int ClientAppPort = 3000;
+    public const string ClientAppFolder = "ClientApp";
+    public const string ClientAppRunCommand = "nuxt dev";
+}
--- GamesTournamentsWeb.Web/Program.cs
+++ GamesTournamentsWeb.Web/Program.cs
@@ -1,3 +1,7 @@
+using GamesTournamentsWeb.Web;
+using GamesTournamentsWeb.Web.Helpers;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,9 +9,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "API",
+        Description = ""
+    });
+
+    options.CustomSchemaIds(type => type.ToString());
+});
+
+builder.Services.AddSpaStaticFiles(options => options.RootPath = $"{Constants.ClientAppFolder}/dist");
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyMethod().AllowAnyOrigin();
+        });
+});
 
 var app = builder.Build();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,5 +48,16 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSpaStaticFiles();
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = Constants.ClientAppFolder;
+    if (app.Environment.IsDevelopment())
+    {
+        // Launch development server for Nuxt
+        spa.UseNuxtDevelopmentServer();
+    }
+});
 
 app.Run();
