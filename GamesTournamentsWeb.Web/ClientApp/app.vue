28013e8b3880f6ab9a65cd343aac5b5c55c89e94
xxxmoney <xxxmoney111@gmail.com>
2024-01-04 08:22:41 +0100

    Functional primevue with tailwind, presets, other.

--- GamesTournamentsWeb.Web/ClientApp/app.vue
+++ GamesTournamentsWeb.Web/ClientApp/app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <InputText value="fffs" />
   </div>
 </template>
--- GamesTournamentsWeb.Web/ClientApp/nuxt.config.ts
+++ GamesTournamentsWeb.Web/ClientApp/nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+      "@nuxtjs/tailwindcss",
+      "nuxt-primevue"
+  ],
+  primevue: {
+    unstyled: true,
+    importPT: { from: '@/presets/wind/', as: 'wind' },
+  },
+  css: ['~/assets/css/main.css']
 })
