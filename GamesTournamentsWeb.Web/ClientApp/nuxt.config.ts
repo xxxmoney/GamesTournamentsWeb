6d5185ba0a2bc154b9ba3eb99363647db3599b58
xxxmoney <xxxmoney111@gmail.com>
2024-01-04 10:05:30 +0100

    Smol eslint change.

--- GamesTournamentsWeb.Web/ClientApp/nuxt.config.ts
+++ GamesTournamentsWeb.Web/ClientApp/nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-    devtools: {enabled: true},
-    modules: [
-        "@nuxtjs/tailwindcss",
-        "nuxt-primevue",
-        "@nuxtjs/eslint-module"
-    ],
-    primevue: {
-        unstyled: true,
-        importPT: {from: '@/presets/wind/', as: 'wind'},
-    },
-    css: ['~/assets/css/main.css']
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt-primevue',
+    '@nuxtjs/eslint-module'
+  ],
+  primevue: {
+    unstyled: true,
+    importPT: { from: '@/presets/wind/', as: 'wind' }
+  },
+  css: ['~/assets/css/main.css']
 })
