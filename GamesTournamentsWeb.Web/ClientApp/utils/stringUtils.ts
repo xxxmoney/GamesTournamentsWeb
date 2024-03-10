7a5188fe3d93447fb853e7b71496cca0eaab68ca
xxxmoney <xxxmoney111@gmail.com>
2024-03-10 13:59:17 +0100

    Snake case

--- GamesTournamentsWeb.Web/ClientApp/utils/stringUtils.ts
+++ GamesTournamentsWeb.Web/ClientApp/utils/stringUtils.ts
@@ -1,5 +1,5 @@
-const toKebabCase = (str: string): string => {
-  return str.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase()
+const toSnakeCase = (str: string): string => {
+  return str.replace(/([a-z0-9])([A-Z])/g, '$1_$2').toLowerCase()
 }
 
-export { toKebabCase }
+export { toSnakeCase }
