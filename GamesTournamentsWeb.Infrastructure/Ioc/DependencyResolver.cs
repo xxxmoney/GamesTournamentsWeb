3ec9e8d613b51e3751bc2f2ff4cd5e135093de87
xxxmoney <xxxmoney111@gmail.com>
2024-03-16 14:13:30 +0100

    Test project setup, IOC wip

---
+++ GamesTournamentsWeb.Infrastructure/Ioc/DependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GamesTournamentsWeb.Infrastructure.Ioc;
+
+public static class DependencyResolver
+{
+    public static IServiceCollection ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        // TODO: Add services here
+        
+        // services.AddAutoMapper(cfg =>
+        //     {
+        //         cfg.AddCollectionMappers();
+        //     },
+        //     AppDomain.CurrentDomain.GetAssemblies()
+        // );
+        
+        // Serilog.Log.Logger = new LoggerConfiguration()
+        //     .Enrich.FromLogContext()
+        //     .WriteTo.MSSqlServer(
+        //         connectionString: configuration.GetConnectionString("ProductionTools"),
+        //         columnOptionsSection: configuration.GetSection("Serilog:ColumnOptions"),
+        //         sinkOptions: new MSSqlServerSinkOptions
+        //         {
+        //             AutoCreateSqlTable = true,
+        //             TableName = configuration.GetSection("Serilog:TableName").Value,
+        //             SchemaName = configuration.GetSection("Serilog:SchemaName").Value
+        //         })
+        //     .CreateLogger();
+
+
+        return services;
+    }
+}
