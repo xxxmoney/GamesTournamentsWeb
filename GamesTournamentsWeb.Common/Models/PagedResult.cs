debffb035f82c6b6881833c90541cbae48f46f59
xxxmoney <xxxmoney111@gmail.com>
2024-03-16 17:33:22 +0100

    WIP game repo

---
+++ GamesTournamentsWeb.Common/Models/PagedResult.cs
@@ -0,0 +1,11 @@
+﻿namespace GamesTournamentsWeb.Common.Models;
+
+public class PagedResult<T>
+    where T : class, new()
+{
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
+    public int RowCount { get; set; }
+    public int PageCount { get; set; }
+    public List<T> Results { get; set; }
+}
---
+++ GamesTournamentsWeb.DataAccess/Repositories/GameRepository.cs
@@ -0,0 +1,30 @@
+﻿using GamesTournamentsWeb.Common.Models;
+using GamesTournamentsWeb.DataAccess.Extensions;
+using GamesTournamentsWeb.DataAccess.Models.Games;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamesTournamentsWeb.DataAccess.Repositories;
+
+public interface IGameRepository
+{
+    Task<PagedResult<GameOverview>> GetGameOverviewsAsync(int page, int count);
+    
+    Task<Game> GetGameByIdAsync(int id);
+}
+
+public class GameRepository(DbSet<Game> games) : IGameRepository
+{
+    public async Task<PagedResult<GameOverview>> GetGameOverviewsAsync(int page, int count)
+    {
+        return await games.Select(g => new GameOverview
+        {
+            Id = g.Id,
+            Name = g.Name
+        }).ToPagedAsync(page, count);
+    }
+
+    public async Task<Game> GetGameByIdAsync(int id)
+    {
+        return await games.FindAsync(id);
+    }
+}
