efd5e0b7070450dff4fb3809aba7e96f8e212087
xxxmoney <xxxmoney111@gmail.com>
2024-04-19 15:54:58 +0200

    Tournament player expected winner migration

--- GamesTournamentsWeb.DataAccess/Models/Tournaments/TournamentPlayer.cs
+++ GamesTournamentsWeb.DataAccess/Models/Tournaments/TournamentPlayer.cs
@@ -9,6 +9,7 @@ public class TournamentPlayer
     public Tournament Tournament { get; set; }
     public int AccountId { get; set; }
     public Account Account { get; set; }
+    public int? ExpectedWinnerId { get; set; }
     public string GameUsername { get; set; }
     public int StatusId { get; set; }
     public TournamentPlayerStatus Status { get; set; }
