c90758ca4896844df16b50154603bfe797a50d49
xxxmoney <xxxmoney111@gmail.com>
2024-04-20 10:42:14 +0200

    Win loss ratio module migration

---
+++ GamesTournamentsWeb.DataAccess/Migrations/20240420084129_AddWinLossRatioModule.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamesTournamentsWeb.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWinLossRatioModule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "enum",
+                table: "Module",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "WinLossRatio" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "enum",
+                table: "Module",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
